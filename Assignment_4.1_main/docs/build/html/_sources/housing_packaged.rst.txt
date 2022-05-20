b50d3d8d573f5578bea9dc1439259b02c48d84d7
PurvaPatidar-Tiger <purva.patidar@tigeranalytics.com>
2022-05-20 16:49:08 +0530

    new folder for assignment 4.1 added

---
+++ Assignment_4.1_main/docs/build/html/_sources/housing_packaged.rst.txt
@@ -0,0 +1,53 @@
+housing\_packaged package
+=========================
+
+Submodules
+----------
+
+housing\_packaged.ingest\_data module
+-------------------------------------
+
+.. automodule:: housing_packaged.ingest_data
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+housing\_packaged.logger module
+-------------------------------
+
+.. automodule:: housing_packaged.logger
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+housing\_packaged.main module
+-----------------------------
+
+.. automodule:: housing_packaged.main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+housing\_packaged.score module
+------------------------------
+
+.. automodule:: housing_packaged.score
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+housing\_packaged.train module
+------------------------------
+
+.. automodule:: housing_packaged.train
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: housing_packaged
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ Assignment_4.1_main/test/train_test.py
@@ -0,0 +1,48 @@
+"""
+This module contains unit tests for housing_packaged/train.py.
+"""
+import os
+
+from sklearn.svm import SVR
+
+# from housing_packaged import train as train
+import housing_packaged.train as train
+
+# import train as train
+from housing_packaged.logger import configure_logger
+
+args = train.parse_args()
+logger = configure_logger()
+
+
+def test_parse_args():
+    """
+    Tests parse_args function.
+    """
+    assert args.dataset == "data/processed/housing_train.csv"
+    assert args.models == "data/models/"
+    assert args.log_level == "DEBUG"
+    assert not args.no_console_log
+    assert args.log_path == "log_file.txt"
+
+
+def test_load_data():
+    """
+    Tests load_data function.
+    """
+    X, y = train.load_data(args.dataset)
+    assert len(X) == len(y)
+    assert "median_house_value" not in X.columns
+    assert not X.isna().sum().sum()
+    assert len(y.shape) == 1
+
+
+def test_save():
+    """
+    Tests save_model function.
+    """
+    svr = SVR()
+    train.save_model(svr, args.models)
+    name = type(svr).__name__
+    assert os.path.isfile(f"{args.models}/{name}.pkl")
+    os.remove(f"{args.models}/{name}.pkl")
